865379729b78c5e6460f2061ffb00f42a3ad3f2b
Mikkel Madsen <m@madsn.net>
2014-03-09 17:45:16 +0100

    plus/minus beer and soda buttons increment counters

--- Bartender/Bartender/src/com/systematic/android/bartender/MainActivity.java
+++ Bartender/Bartender/src/com/systematic/android/bartender/MainActivity.java
@@ -2,14 +2,28 @@ package com.systematic.android.bartender;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	TextView sodaCount, beerCount;
+	EditText initials;
+	Bartender bartender;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		bartender = new Bartender();
+		
+		sodaCount = (TextView) findViewById(R.id.soda_count);
+		beerCount = (TextView) findViewById(R.id.beer_count);
+		initials = (EditText) findViewById(R.id.initials_edittext);
 	}
 
 	@Override
@@ -18,5 +32,28 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void onBeerPlusBtnClick(View v){
+		bartender.addBeer();
+		beerCount.setText(bartender.getBeerCountAsString());
+	}
+	
+	public void onSodaPlusBtnClick(View v){
+		bartender.addSoda();
+		sodaCount.setText(bartender.getSodaCountAsString());
+	}
+	
+	public void onBeerMinusBtnClick(View v){
+		bartender.removeBeer();
+		beerCount.setText(bartender.getBeerCountAsString());
+	}
+	
+	public void onSodaMinusBtnClick(View v){
+		bartender.removeSoda();
+		sodaCount.setText(bartender.getSodaCountAsString());
+	}
+	
+	public void onSaveBtnClick(View v){
+		
+	}
 }
